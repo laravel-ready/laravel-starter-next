a7ac1c4d961076d4c87c636cfc09e81e805a1b38
Eyüp <epsilonelittr@gmail.com>
2025-06-11 14:00:58 +0300

    Add pretty routes package

---
+++ resources/views/vendor/pretty-routes/styles.blade.php
@@ -0,0 +1,42 @@
+<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+
+<style>
+    .spaced { margin: 2px; }
+
+    .deprecated { text-decoration: line-through; }
+
+    .link:hover { text-decoration: underline; cursor: pointer; }
+
+    .rotate {
+        animation: spin 4s linear infinite;
+    }
+
+    @-moz-keyframes spin {
+        100% {
+            -moz-transform: rotate(360deg);
+        }
+    }
+
+    @-webkit-keyframes spin {
+        100% {
+            -webkit-transform: rotate(360deg);
+        }
+    }
+
+    @keyframes spin {
+        100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+        }
+    }
+
+    .text-decoration-none{
+        text-decoration: none;
+    }
+
+    .mr-5px{
+        margin-right: 5px;
+    }
+</style>
