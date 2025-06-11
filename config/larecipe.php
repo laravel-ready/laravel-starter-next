8407673e46b2180877e9347febdd80adc653ca95
Eyüp <epsilonelittr@gmail.com>
2025-06-11 14:52:46 +0300

    Update larecipe configs

--- config/larecipe.php
+++ config/larecipe.php
@@ -95,8 +95,8 @@ return [
     */
 
     'search'            => [
-        'enabled'       => false,
-        'default'       => 'algolia',
+        'enabled'       => true,
+        'default'       => 'internal',
         'engines'       => [
             'internal'  => [
                 'index' => ['h2', 'h3']
