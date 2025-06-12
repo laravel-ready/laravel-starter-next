c55f2209ab6db4b1864f65cd18cae9fa116e0e0b
Eyüp <epsilonelittr@gmail.com>
2025-06-13 01:15:25 +0300

    Add local scripts

---
+++ .scripts/local/install.bat
@@ -0,0 +1,22 @@
+@REM copy .env.example as .env
+CALL copy .env.example .env
+
+@REM install PHP packges
+CALL composer install
+
+@REM laravel app key
+CALL php artisan key:generate
+
+@REM link storage folder
+CALL php artisan storage:link
+
+@REM install NPM packges
+CALL pnpm i
+
+@REM run docker images
+CALL docker compose -f docker-compose-dev.yml up -d
+
+@REM setup project
+CALL docker exec tc_dev_app composer install
+CALL docker exec tc_dev_app php artisan key:generate
+CALL docker exec tc_dev_app php artisan migrate:fresh --seed
---
+++ .scripts/local/install.sh
@@ -0,0 +1,22 @@
+# copy .env.example as .env
+cp .env.example .env
+
+# install PHP packges
+composer install
+
+# laravel app key
+php artisan key:generate
+
+# link storage folder
+php artisan storage:link
+
+# install NPM packges
+pnpm i
+
+# run docker images
+docker compose -f docker-compose-dev.yml up -d
+
+# setup project
+docker exec tc_dev_app composer install
+docker exec tc_dev_app php artisan key:generate
+docker exec tc_dev_app php artisan migrate:fresh --seed
